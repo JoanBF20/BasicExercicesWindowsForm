5a7687b8967b38eb9c61631ab78f18af0bb98ec0
Joan Barcelo Fiol <joanbarcelo1@paucasesnovescifp.cat>
2020-10-14 19:52:40 +0200

    Created a project with a simple form to sum two numbers
    Created a project with Visual Studio that contains a simple form to sum two numbers.
    The form contains 3 textboxs, 2 for the operands and 1 for the result, one button to sum the two numbers and show the result on the corresponent textbox, and another button to reset the content of all the textboxes

---
+++ Exercise 3/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Exercise_3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int res = Convert.ToInt32(textBox1.Text) + Convert.ToInt32(textBox2.Text);
+                textBox3.Text = res.ToString();
+            } catch
+            {
+
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.ResetText();
+            textBox2.ResetText();
+            textBox3.ResetText();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
