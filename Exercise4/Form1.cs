eabd786cc97e4ee20f7f4a6b0ef45e0d0c5acb0f
Joan Barcelo Fiol <joanbarcelo1@paucasesnovescifp.cat>
2020-10-14 20:36:37 +0200

    Created a project with a simple form to sum or substract two numbers
    Created a project with Visual Studio that contains a simple form to sum or substract two numbers.
    The form contains 4 textboxs, 2 for the operands, 1 to show the seleccted operation and 1 for the result, 2 buttons to selecct the operation (Sum or Substract), 1 button to calculate the result and show it on the corresponent textbox, and another button to reset the content of all the textboxes

---
+++ Exercise4/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Exercise4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox3.Text = "+";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox3.Text = "-";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            textBox1.ResetText();
+            textBox2.ResetText();
+            textBox4.ResetText();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (textBox3.Text == "+")
+                {
+                    int res = Convert.ToInt32(textBox1.Text) + Convert.ToInt32(textBox2.Text);
+                    textBox4.Text = res.ToString();
+                } else if (textBox3.Text == "-")
+                {
+                    int res = Convert.ToInt32(textBox1.Text) - Convert.ToInt32(textBox2.Text);
+                    textBox4.Text = res.ToString();
+                } 
+            }
+            catch
+            {
+
+            }
+        }
+    }
+}
