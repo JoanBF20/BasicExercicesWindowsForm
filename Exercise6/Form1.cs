6d3d3b14fc1ff37b902e37da820894e4d9fe96b1
Joan Barcelo Fiol <joanbarcelo1@paucasesnovescifp.cat>
2020-10-15 19:22:06 +0200

    Created a project with a simple form with a 3 radiobuttons
    Created a project with Visual Studio that contains a simple form with 3 radiobuttons.
    When you click the button "Ok" it shows the selected color on the TextBox.

---
+++ Exercise6/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Exercise6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked)
+                textBox1.Text = "Choosen Colour: Red";
+
+            if (radioButton2.Checked)
+                textBox1.Text = "Choosen Colour: Blue";
+
+            if (radioButton3.Checked)
+                textBox1.Text = "Choosen Colour: White";
+        }
+    }
+}
