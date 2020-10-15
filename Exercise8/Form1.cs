4da5e85cf0bf68b6f3f80a9b15aae0ecc57853c3
Joan Barcelo Fiol <joanbarcelo1@paucasesnovescifp.cat>
2020-10-15 20:11:40 +0200

    Created a project with a simple form with  a listbox, two buttons and a textbox. Created a project with Visual Studio that contains a simple form with  a listbox, two buttons and a textbox. When you click on "Classroom 1", all the students of classroom 1 are charged into the listbox. When you click on "Classroom 2", all the students of classroom 2 are charged into the listbox. The button ‘Clear List’ clears the listbox. And when you click on some student, this student is copied into the textbox.

---
+++ Exercise8/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Exercise8
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int count = listBox1.Items.Count;
+            for (int i = 0; i < count; i++)
+                listBox1.Items.RemoveAt(0);
+
+            listBox1.Items.Add("Pau");
+            listBox1.Items.Add("Montse");
+            listBox1.Items.Add("Jaume");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int count = listBox1.Items.Count;
+            for (int i = 0; i < count; i++)
+                listBox1.Items.RemoveAt(0);
+
+            listBox1.Items.Add("Cati");
+            listBox1.Items.Add("Pau");
+            listBox1.Items.Add("Maria");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int count = listBox1.Items.Count;
+            for (int i = 0; i < count; i++)
+                listBox1.Items.RemoveAt(0);
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            textBox1.Text = listBox1.Text;
+        }
+    }
+}
