94cdc8b0d9da9b2a7227098b421c0acc5d023227
Joan Barcelo Fiol <joanbarcelo1@paucasesnovescifp.cat>
2020-10-15 19:36:04 +0200

    Created a project with a simple form with a combobox of 5 colours, and a textbox
    Created a project with Visual Studio that contains a simple form with a combobox of 5 colours, and a textbox.
    When you select a colour appears the message “The choosen colour is: " and the selected colour on the textbox.

---
+++ Exercise7/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Exercise7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (comboBox1.Text)
+            {
+                case "Red":
+                    textBox1.Text = "The choosen colour is: Red";
+                    break;
+                case "Green":
+                    textBox1.Text = "The choosen colour is: Green";
+                    break;
+                case "Blue":
+                    textBox1.Text = "The choosen colour is: Blue";
+                    break;
+                case "Yellow":
+                    textBox1.Text = "The choosen colour is: Yellow";
+                    break;
+                case "White":
+                    textBox1.Text = "The choosen colour is: White";
+                    break;
+                default:
+                    textBox1.Text = "";
+                    break;
+            }
+        }
+    }
+}
