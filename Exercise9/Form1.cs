3fb3f47cd1b3d9e22d53c945488b51b6c7de2b52
Joan Barcelo Fiol <joanbarcelo1@paucasesnovescifp.cat>
2020-10-16 18:29:04 +0200

    Created a project with a simple form with a combobox, two buttons and a textbox.
    Created a project with Visual Studio that contains a simple form with  a combobox, two buttons and a textbox.
    When you click on "Even (par)", charge even number from 0 to 10 on the combobox.
    When you click on "Odd (impar)", charge odd number from 0 to 10 on the combobox.
    And when you select a number on the comboboxt, the number is copied into the textbox.

---
+++ Exercise9/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Exercise9
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int count = comboBox1.Items.Count;
+            for (int i = 0; i < count; i++)
+                comboBox1.Items.RemoveAt(0);
+
+            comboBox1.Items.Add("2");
+            comboBox1.Items.Add("4");
+            comboBox1.Items.Add("6");
+            comboBox1.Items.Add("8");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int count = comboBox1.Items.Count;
+            for (int i = 0; i < count; i++)
+                comboBox1.Items.RemoveAt(0);
+
+            comboBox1.Items.Add("1");
+            comboBox1.Items.Add("3");
+            comboBox1.Items.Add("5");
+            comboBox1.Items.Add("7");
+            comboBox1.Items.Add("9");
+        }
+
+        private void comboBox1_SelectedValueChanged(object sender, EventArgs e)
+        {
+            textBox1.Text = comboBox1.Text;
+        }
+    }
+}
